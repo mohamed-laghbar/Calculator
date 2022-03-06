818cde5b0c28560d25a8cd063fa2087cd50af622
Mohamed Laghabr <93931981+Mohamed-Laghbar@users.noreply.github.com>
2022-03-06 11:20:15 +0100

    done

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 var screen = document.getElementById("screen");
+var numchar, previousNum, currentNum;
+var operations = ["-", "+", "/", "*"];
 
 function display(c) {
   screen.value += c;
+  numchar = screen.value.length;
+  currentNum = c;
+  getPrevious();
+  checkSyntax()
 }
 
 function clearScreen() {
@@ -9,9 +15,24 @@ function clearScreen() {
 }
 
 function clearElement() {
-  screen.value = screen.value;
+  screen.value = screen.value.slice(0, -1);
 }
 
 function equal() {
   screen.value = eval(screen.value);
+}
+
+function getPrevious(){
+previousNum = screen.value.substring(numchar-2, numchar-1);
+
+}
+
+function checkSyntax(){
+if (operations.includes(previousNum) && operations.includes(currentNum)){
+    if(previousNum == currentNum){
+      screen.value = screen.value.substring(0,numchar-1);
+    }else{
+      screen.value = screen.value.slice(0,numchar-2) + screen.value.slice(numchar-1);
+    }
+}
 }
