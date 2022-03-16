a77d0cb98502f58929598fa858ee588354da21ee
Mohamed Laghabr <93931981+Mohamed-Laghbar@users.noreply.github.com>
2022-03-16 11:28:20 +0100

    Last modifications

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 var screen = document.getElementById("screen");
 var numchar, previousNum, currentNum;
-var operations = ["-", "+", "/", "*"];
+var operations = ["-", "+", "/", "*", "%" , "."];
+
+
 
 function display(c) {
+
+  if(operations.includes(c) && screen.value.length== null){
+    screen.value="";
+  }
   screen.value += c;
   numchar = screen.value.length;
   currentNum = c;
+  
   getPrevious();
-  checkSyntax()
+  checkSyntax();
 }
 
 function clearScreen() {
@@ -22,25 +29,36 @@ function equal() {
   screen.value = eval(screen.value);
 }
 
-function getPrevious(){
-previousNum = screen.value.substring(numchar-2, numchar-1);
-
+function getPrevious() {
+  previousNum = screen.value.substring(numchar - 2, numchar - 1);
 }
 
-function checkSyntax(){
-if (operations.includes(previousNum) && operations.includes(currentNum)){
-    if(previousNum == currentNum){
-      screen.value = screen.value.substring(0,numchar-1);
-    }else{
-      screen.value = screen.value.slice(0,numchar-2) + screen.value.slice(numchar-1);
+function checkSyntax() {
+  if (operations.includes(previousNum) && operations.includes(currentNum)) {
+    if (previousNum == currentNum) {
+      screen.value = screen.value.substring(0, numchar - 1);
+    } else {
+      screen.value =
+        screen.value.slice(0, numchar - 2) + screen.value.slice(numchar - 1);
     }
-}
+  }
 
-if (previousNum=='.'  && currentNum=='.'){
-  screen.value = screen.value.substring(0,numchar-1);
-}
+  if (previousNum == "." && currentNum == ".") {
+    screen.value = screen.value.substring(0, numchar - 1);
+  }
 
-if(previousNum=='/' && currentNum=='0'){
-  screen.value = 'A Sab7aan Lah 😅';
-}
+  
+
+  if (previousNum == "/" && currentNum == "0") {
+    screen.value = "A Sab7aan Lah 😅";
+  }
+
+
+  if(previousNum == ''  && operations.includes(currentNum)){
+    screen.value = '';
+  }
+
+  
+
+  
 }
